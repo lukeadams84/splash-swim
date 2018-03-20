f095804d6fbcd5c17cbb539d27cd0a1534673b3f
Luke Adams <lu.adams@mlwes56069.london.net-a-porter.com>
2018-03-20 13:38:23 +0000

    updates to stripe

--- app/vendor/stripe/stripe/config/bootstrap.php
+++ app/vendor/stripe/stripe/config/bootstrap.php
@@ -5,14 +5,13 @@ use Cake\Core\Plugin;
 
 namespace Stripe;
 
-require_once("../vendor/stripe/stripe/init.php");
-
+require_once("../vendor/stripe/stripe-php/init.php");
 
 $stripe = array(
   "secret_key"      => "sk_test_6dnaLV4sdSU2OLxtizBpXXDO",
   "publishable_key" => "pk_test_ARgAJVgJV0Qd8Dau7ROW6Tth"
 );
 
-Stripe::setApiKey($stripe['secret_key']);
+\Stripe\Stripe::setApiKey($stripe['secret_key']);
 
 ?>
